b193484656b37a0fbe29d05afd1d88f5a929d559
TRAKOS CSI <69827418+mhdeiiking@users.noreply.github.com>
2022-02-23 18:58:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import requests
-import uuid
-from uuid import uuid4
 import telebot
-token = "5247038079:AAFVfl9iJnqRVxnBS08R7h9WVqWCwbIsgsQ"
-bot = telebot.TeleBot(token)
+import uuid
+import requests
+from uuid import uuid4
+
+bot = telebot.TeleBot("5247038079:AAFVfl9iJnqRVxnBS08R7h9WVqWCwbIsgsQ")
 url = 'https://i.instagram.com/api/v1/accounts/login/'
 headers = {'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440x2416; huawei/google; Nexus 6P; angler; angler; en_US)',
         'Accept': "*/*",
@@ -14,19 +14,24 @@ headers = {'User-Agent': 'Instagram 113.0.0.39.122 Android (24/5.0; 515dpi; 1440
         'X-IG-Connection-Type': 'WIFI',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Host': 'i.instagram.com'}
-@bot.message_handler(commands=["start"])
-def login(message):
-    bot.send_message(message.chat.id,f"<strong>Hi ,\n== === ==\nWellcome to Login Instagram Bot\nSend User:pass Now !\n== === ==\nBy : @trprogram</strong>",parse_mode="html")
-@bot.message_handler(func=lambda message:True)
-def loginNow(message):
-    msg = message.text
-    bot.send_message(message.chat.id,"Wait ..")
-    username = msg.split(":")[0]
-    password = msg.split(":")[1]
+@bot.message_handler(commands=['start'])
+def start(message):
+    if message.chat.type == "private":
+        bot.send_message(message.chat.id,text=f"<strong>- Hi , Send Your Username Now!\n===========\n- @trprogram</strong>",parse_mode="html")
+
+@bot.message_handler(func=lambda m:True)
+def mounth(message):
+    global msg1,sent
+    msg1 = message.text
+    sent = bot.send_message(message.chat.id, text=f"<strong>- Send Password Now!</strong>",parse_mode="html")
+    bot.register_next_step_handler(sent, send)
+
+def send(message):
+    pp = message.text
     uid = str(uuid4())
     data = {
-        "password":password,
-        "username":username,
+        "password":pp,
+        "username":msg1,
         "device_id":uid,
         "from_req":"false",
         '_csrftoken': 'missing',
@@ -43,4 +48,8 @@ def loginNow(message):
         bot.send_message(message.chat.id, f"<strong>Done Login Sir , But Secureid Account ..</strong>", parse_mode="html")
     if "bad_password" in req.text:
         bot.send_message(message.chat.id,f"<strong>Error Login Sir .</strong>",parse_mode="html")
-bot.polling()
+    else:
+    	bot.send_message(message.chat.id,"Error")
+    
+
+bot.infinity_polling()
